49f621d94f1f459b905dec903e3ea546ec1c9d26
cyborgNjigu <mwaura.njigu@gmail.com>
2022-07-19 15:07:00 +0300

    wrap in loop for display list 10 times

--- 001-functions_and_loops/many-functions.c
+++ 001-functions_and_loops/many-functions.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 /**
- * @main - display alphabet using ASCII codes
+ * @main - display alphabet using ASCII codes and global functions
+ *
  *
  * return : Always 0
  */
@@ -9,10 +10,19 @@ int main(void)
 {
 	/*declare variables*/
 	char alphabet;
+	int count  = 1;
 
 	/*call function*/
+	/*display the list 10 times*/
+	while (count <= 10)
+	{
 	letters(alphabet);
+	printf("  %d ", count);
+
+	count++;
 	printf("\n");
+	}
+
 	return (0);
 }
 /**
@@ -22,6 +32,6 @@ void letters(char alpha)
 {
 	for(alpha = 97; alpha <= 122; alpha++)
 	{
-		printf("%c ", alpha);
+		printf(" %c ", alpha);
 	}
 }
