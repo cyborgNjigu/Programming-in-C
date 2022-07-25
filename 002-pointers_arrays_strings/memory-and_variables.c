1e363d52abf4450156ca5d44ac860f0f6b532111
cyborgNjigu <mwaura.njigu@gmail.com>
2022-07-25 12:38:43 +0300

    Display data type sizes

---
+++ 002-pointers_arrays_strings/memory-and_variables.c
@@ -0,0 +1,27 @@
+/*All about memory*/
+#include <stdio.h>
+/**
+ * @Main- Summarise on memory and varibles
+ *
+ * @return-Always zero.
+ */
+int main(void)
+{
+	/*display the sizes of the data types in C for a 64 bit computer*/
+	printf("\n");
+	printf("Size of type 'char' is: %lu bytes\n", sizeof(char));
+	printf("Size of type 'int' is: %lu bytes\n", sizeof(int));
+	printf("Size of type 'short int' is: %lu bytes\n", sizeof(short int));
+	printf("Size of type 'float' is: %lu bytes\n", sizeof(float));
+	printf("Size of type 'double' is: %lu bytes\n", sizeof(double));
+	printf("Size of type 'void' is: %lu bytes\n", sizeof(void));
+	printf("Size of type 'long' is: %lu bytes\n", sizeof(long));
+	printf("Size of type 'unsigned long'is: %lu bytes\n", sizeof(unsigned long));
+	printf("Size of type 'long double' is: %lu bytes\n", sizeof(long double));
+	printf("\n");
+	
+	/*Calculate the possible values a type can hold*/
+	
+	printf("\n");
+	return (0);
+}
